870b4b53ea9b8a2115d6cde52c2fe64f92029805
Niels van Dijk <niels-van-dijk@hotmail.com>
2025-03-18 23:13:26 +0100

    Fix bracket

--- Pdfgenerator/PdfGenerator/Program.cs
+++ Pdfgenerator/PdfGenerator/Program.cs
@@ -22,102 +22,101 @@ class Program
 
 
         string jsonDataHafkamp = @"
-{
-  ""persoon"": {
-    ""identiteit"": {
-      ""voornaam"": ""Andre"",
-      ""tussenvoegsel"": ""Van der"",
-      ""achternaam"": ""Kamp"",
-      ""geboortedatum"": ""1978-03-01"",
-      ""nationaliteit"": ""Nederlands""
-    },
-    ""contactgegevens"": {
-      ""email"": ""test@email.com"",
-      ""telefoonnummer"": ""0612345678""
-    },
-    ""adres"": {
-      ""straat"": ""Eusebiusstraat"",
-      ""huisnummer"": ""1"",
-      ""postcode"": ""6846XX"",
-      ""plaats"": ""Arnhem""
-    },
-    ""huishouden"": {
-      ""burgerlijke_staat"": ""Getrouwd"",
-      ""geregistreerde_partner"": 1,
-      ""inwonende_kinderen"": 2
-    }
-  },
-  ""financien"": {
-    ""besteedbaar_inkomen_per_maand"": 101,
-    ""periode"": {
-      ""start"": ""2024-12"",
-      ""eind"": ""2025-02""
-    },
-    ""inkomen"": {
-      ""loondienst"": 1565,
-      ""toeslagen"": 366,
-      ""overige_inkomsten"": 156,
-      ""totaal"": 2322
-    },
-    ""uitgaven"": {
-      ""vast"": 1100,
-      ""variabel"": 655,
-      ""vrij"": 466,
-      ""totaal"": 2221
-    },
-    ""maandelijks_totaal"": {
-      ""december_24"": 2586,
-      ""januari_25"": 2645,
-      ""februari_25"": 2610
-    }
-  },
-  ""inkomensspecificatie"": {
-    ""periode"": ""2025-02"",
-    ""loondienst"": {
-      ""sv_loon"": 1800,
-      ""werkgever"": ""Werk B.V."",
-      ""type_inkomen"": ""Loondienst"",
-      ""type_verloning"": ""Maandelijks"",
-      ""netto_inkomen"": 1565
-    },
-    ""uitkering"": ""nvt"",
-    ""alimentatie"": ""nvt"",
-    ""toeslagen"": {
-      ""huurtoeslag"": 256,
-      ""zorgtoeslag"": 110
-    },
-    ""overige_inkomsten"": {
-      ""cash_storting"": 456
-    }
-  },
-  ""uitgavenspecificatie"": {
-    ""periode"": ""2025-02"",
-    ""vast"": {
-      ""wonen"": 1800,
-      ""energie_water"": 1565,
-      ""gemeentelijke_belastingen"": 1565,
-      ""communicatie_media"": 1565,
-      ""verzekeringen"": 1565
-    },
-    ""variabel"": {},
-    ""vrij"": ""nvt"",
-    ""overig"": {
-      ""kinderopvang"": 256,
-      ""vervoer"": 110,
-      ""schulden_leningen"": 110,
-      ""alimentatie"": 110
-    },
-    ""toeslagen"": {
-      ""huurtoeslag"": 256,
-      ""zorgtoeslag"": 110
-    },
-    ""overige_inkomsten"": {
-      ""cash_storting"": 456
-    }
-  }
-}
-
-"
+        {
+          ""persoon"": {
+            ""identiteit"": {
+              ""voornaam"": ""Andre"",
+              ""tussenvoegsel"": ""Van der"",
+              ""achternaam"": ""Kamp"",
+              ""geboortedatum"": ""1978-03-01"",
+              ""nationaliteit"": ""Nederlands""
+            },
+            ""contactgegevens"": {
+              ""email"": ""test@email.com"",
+              ""telefoonnummer"": ""0612345678""
+            },
+            ""adres"": {
+              ""straat"": ""Eusebiusstraat"",
+              ""huisnummer"": ""1"",
+              ""postcode"": ""6846XX"",
+              ""plaats"": ""Arnhem""
+            },
+            ""huishouden"": {
+              ""burgerlijke_staat"": ""Getrouwd"",
+              ""geregistreerde_partner"": 1,
+              ""inwonende_kinderen"": 2
+            }
+          },
+          ""financien"": {
+            ""besteedbaar_inkomen_per_maand"": 101,
+            ""periode"": {
+              ""start"": ""2024-12"",
+              ""eind"": ""2025-02""
+            },
+            ""inkomen"": {
+              ""loondienst"": 1565,
+              ""toeslagen"": 366,
+              ""overige_inkomsten"": 156,
+              ""totaal"": 2322
+            },
+            ""uitgaven"": {
+              ""vast"": 1100,
+              ""variabel"": 655,
+              ""vrij"": 466,
+              ""totaal"": 2221
+            },
+            ""maandelijks_totaal"": {
+              ""december_24"": 2586,
+              ""januari_25"": 2645,
+              ""februari_25"": 2610
+            }
+          },
+          ""inkomensspecificatie"": {
+            ""periode"": ""2025-02"",
+            ""loondienst"": {
+              ""sv_loon"": 1800,
+              ""werkgever"": ""Werk B.V."",
+              ""type_inkomen"": ""Loondienst"",
+              ""type_verloning"": ""Maandelijks"",
+              ""netto_inkomen"": 1565
+            },
+            ""uitkering"": ""nvt"",
+            ""alimentatie"": ""nvt"",
+            ""toeslagen"": {
+              ""huurtoeslag"": 256,
+              ""zorgtoeslag"": 110
+            },
+            ""overige_inkomsten"": {
+              ""cash_storting"": 456
+            }
+          },
+          ""uitgavenspecificatie"": {
+            ""periode"": ""2025-02"",
+            ""vast"": {
+              ""wonen"": 1800,
+              ""energie_water"": 1565,
+              ""gemeentelijke_belastingen"": 1565,
+              ""communicatie_media"": 1565,
+              ""verzekeringen"": 1565
+            },
+            ""variabel"": {},
+            ""vrij"": ""nvt"",
+            ""overig"": {
+              ""kinderopvang"": 256,
+              ""vervoer"": 110,
+              ""schulden_leningen"": 110,
+              ""alimentatie"": 110
+            },
+            ""toeslagen"": {
+              ""huurtoeslag"": 256,
+              ""zorgtoeslag"": 110
+            },
+            ""overige_inkomsten"": {
+              ""cash_storting"": 456
+            }
+          }
+        }
+        ";
 
         IPdfGeneratorService pdfGeneratorService = new PdfGeneratorService();
         IPdfTemplate template = new TestPdfTemplate();
