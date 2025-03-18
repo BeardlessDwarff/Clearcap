6db62169e89e002252a657df065ca2691dac572b
Niels van Dijk <niels-van-dijk@hotmail.com>
2025-03-18 20:16:39 +0100

    More cleanup

---
+++ Pdfgenerator/PdfGenerator/Entities/BankAccount.cs
@@ -0,0 +1,8 @@
+namespace PdfGenerator;
+
+public class BankAccount
+{
+    public string AccountNumber { get; set; }
+    public string BankName { get; set; }
+    public double Balance { get; set; }
+}
---
+++ Pdfgenerator/PdfGenerator/Entities/Person.cs
@@ -0,0 +1,9 @@
+namespace PdfGenerator;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Email { get; set; }
+    public BankAccount BankAccount { get; set; }
+}
